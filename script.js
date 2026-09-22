d99de9e2b90c2600445fc08d6f4e8eea0a759a02
253-Mathilde <arianebaby08@gmail.com>
2026-09-22 19:17:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-message = "i can make u dance around me like a hula hoop! ";
+message = "your digital sticker journal...brainstorming....creating....having fun....★ ";
 function step() {
 message = message.substr(1) + message.substr(0,1);
 document.title = message.substr(0,15);
