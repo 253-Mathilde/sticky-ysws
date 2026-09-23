35f5736f0631b0b273c78f40da82f44bbeae4299
253-Mathilde <arianebaby08@gmail.com>
2026-09-23 20:40:08 +0200

    Add files via upload

--- makeScreenshot.js
+++ makeScreenshot.js
@@ -1,21 +1,23 @@
-function postCanvasToURL(data) 
-{
-    data = data.replace('data:image/png;base64,', '');
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', "post.php", true);
-    var boundary = 'this-is-a-string';
-    xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
-    request_str = '--' + boundary + '\r\n' + 'Content-Disposition: form-data; name="formelement"; filename="image.png"' + '\r\n' +
-                   'Content-Type: image/png' + '\r\n\r\n' + atob(data) + '\r\n' + '--' + boundary + '--';
-    var bytes = [];
-    for (var i in request_str)
-    {
-        bytes.push(request_str[i].charCodeAt(0) & 0xff);
-    }
-    xhr.send(new Uint8Array(bytes).buffer);
+function postCanvasToServer(canvas) {
+    canvas.toBlob(function(blob) {
+        var formData = new FormData();
+        formData.append('formelement', blob, 'image.png');
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'post.php', true);
+        
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                console.log('Screenshot erfolgreich gespeichert!');
+            } else {
+                console.error('Fehler beim Server-Upload.');
+            }
+        };
+        xhr.send(formData);
+    }, 'image/png');
 }
-$(window).ready(function()
-{
-    var options = {onrendered : function(canvas) { postCanvasToURL(canvas.toDataURL()); }};
-    var canvasRecord = $('body').html2canvas(options);
+
+$(document).ready(function() {
+    html2canvas(document.body).then(function(canvas) {
+        postCanvasToServer(canvas);
+    });
 });
