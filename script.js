52f2bfc8ab01defde6ee66a1c3af54e1428c78ff
253-Mathilde <arianebaby08@gmail.com>
2026-09-23 18:27:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ function step() {
 message = message.substr(1) + message.substr(0,1);
 document.title = message.substr(0,15);
 }
+
+
+function dragstartHandler(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function dragoverHandler(ev) {
+  ev.preventDefault();
+}
+
+function dropHandler(ev) {
+  ev.preventDefault();
+  const data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
